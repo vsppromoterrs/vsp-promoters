e66213520f557be3992d9b6cee6724f33da28851
Mohan <mohanpraveen4161@gmail.com>
2024-09-18 20:34:57 +0530

    get method added

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,32 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
 
-// Middleware
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://vsppromoter-1794.web.app',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+
+    return callback(null, true);
+  }
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 // Route to send email (implement in step 3)
 app.post('/send-email', require('./emailHandler'));
 
